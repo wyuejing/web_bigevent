efc727df982d8ad7619d62f5c4e6025e4ae46a93
w <1524961608@qq.com>
2021-07-28 01:00:17 +0800

    给登录界面设置了样式

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+    // 点击注册链接
+    $('#link_reg').on('click', function () {
+        $('#login-box').hide();
+        $('#reg-box').show();
+    })
+    // 点击登录链接
+    $('#link_login').on('click', function () {
+        $('#login-box').show();
+        $('#reg-box').hide();
+    })
+})
